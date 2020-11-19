c7f1122ab988ef4972249fc82c1365e45e7d12f9
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-11-19 09:19:35 -0500

    refactored fetching menu items function

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -15,19 +15,20 @@ const getMenu = async () => {
 getMenu()
   .then((data) => {
     const uniqueCategories = [...new Set(data.map((item) => item.category))];
-
     uniqueCategories.forEach((cat) => {
-      if (cat === 'starter') {
-        html = `
-      <div class="menu-heading">
-        <h1 class="menu-title">${cat}</h1>
-        <span class="bar"></span>
-      </div>
-      `;
-        data.forEach((item) => {
-          const { category, title, desc, price } = item;
-          if (category === cat) {
-            html += `
+      const newItems = data.filter((item) => item.category === cat);
+
+      html = `
+          <div class="menu-heading">
+            <h1 class="menu-title">${cat}</h1>
+            <span class="bar"></span>
+          </div>
+        `;
+
+      newItems.forEach((item) => {
+        const { title, desc, price } = item;
+
+        html += `
             
             <div class="menu-item">
               <h2>${title}</h2>
@@ -35,72 +36,7 @@ getMenu()
               <span class="price">$${price}</span>
             </div>     
           `;
-          }
-        });
-      }
-      if (cat === 'soups & salads') {
-        html = `
-      <div class="menu-heading">
-        <h1 class="menu-title">${cat}</h1>
-        <span class="bar"></span>
-      </div>
-      `;
-        data.forEach((item) => {
-          const { category, title, desc, price } = item;
-          if (category === cat) {
-            html += `
-            
-            <div class="menu-item">
-              <h2>${title}</h2>
-              <p>${desc}</p>
-              <span class="price">$${price}</span>
-            </div>     
-          `;
-          }
-        });
-      }
-      if (cat === 'steaks & chops') {
-        html = `
-      <div class="menu-heading">
-        <h1 class="menu-title">${cat}</h1>
-        <span class="bar"></span>
-      </div>
-      `;
-        data.forEach((item) => {
-          const { category, title, desc, price } = item;
-          if (category === cat) {
-            html += `
-            
-            <div class="menu-item">
-              <h2>${title}</h2>
-              <p>${desc}</p>
-              <span class="price">$${price}</span>
-            </div>     
-          `;
-          }
-        });
-      }
-      if (cat === 'favorites') {
-        html = `
-      <div class="menu-heading">
-        <h1 class="menu-title">${cat}</h1>
-        <span class="bar"></span>
-      </div>
-      `;
-        data.forEach((item) => {
-          const { category, title, desc, price } = item;
-          if (category === cat) {
-            html += `
-            
-            <div class="menu-item">
-              <h2>${title}</h2>
-              <p>${desc}</p>
-              <span class="price">$${price}</span>
-            </div>     
-          `;
-          }
-        });
-      }
+      });
       menuList.innerHTML += html;
     });
   })
